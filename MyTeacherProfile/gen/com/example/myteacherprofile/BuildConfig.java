ffe0c83efdb7c1c11c5d9f7ffc067b6d7df94453
Rui Neto <rafnsolutions@gmail.com>
2012-08-03 18:37:53 +0100

    wrong project

--- MyTeacherProfile/gen/com/example/myteacherprofile/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.example.myteacherprofile;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- MyTeacherProfile/src/objects/Sex.java
+++
@@ -1,6 +0,0 @@
-
-package objects;
-
-public enum Sex {
-    Masculino, Feminino
-}
